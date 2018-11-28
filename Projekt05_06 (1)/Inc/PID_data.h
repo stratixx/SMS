0fce25d727011368389e7d115075cc94e5a4bdd1
Konrad Winnicki <darnokusex@wp.pl>
2018-11-28 11:58:05 +0100

    include files after laboratory

--- Projekt05_06 (1)/Inc/PID_data.h
+++ Projekt05_06 (1)/Inc/PID_data.h
@@ -3,17 +3,6 @@
 
 #include <inttypes.h>
 
-/*
-model wyznaczony w Matlabie
-u2 = 0.0159
-u1 = 0.0077
-y2 =-0.6605
-y1 = 1.6368
-z matlaba
-y(k) = u2*u(k-2) + u1*u(k-1) + y2*y(k-2) + y1*y(k-1);
-ze skryptu
-y(k) = 0.043209*u(k-1) + 0.030415*u(k-2) + 1.309644*y(k-1) - 0.346456*y(k-2);
-*/
 
 //Parametry regulatora PID
 
@@ -22,11 +11,34 @@ y(k) = 0.043209*u(k-1) + 0.030415*u(k-2) + 1.309644*y(k-1) - 0.346456*y(k-2);
 #define PID_Tp (1/20.0)
 
 // parametr anti-winding
-#define PID_Tv 0.050f
+#define PID_Tv -8.0f
+
+//% parametry PID zieglerem finalne
+#define PID_Kk 30.0f
+#define PID_Tu (8*PID_Tp)
+
+#define PID_K (0.6*PID_Kk)
+#define PID_Ti (0.5*PID_Tu)
+#define PID_Td (0.12*PID_Tu)
+
+
+//%ręcznie po inżyniersku finalny
+//#define PID_K 15.00f
+//#define PID_Ti 3.5f
+//#define PID_Td 0.040f
+
+
+
+
+
+
+
+
+
 
 //////////////////////////////////////////////////////////////
 //dane wyznaczone na podstawie modelu z Matlaba
-
+/*
 //% parametry PID
 #define PID_Kk 199.33f
 #define PID_Tu 0.1f
@@ -35,7 +47,7 @@ y(k) = 0.043209*u(k-1) + 0.030415*u(k-2) + 1.309644*y(k-1) - 0.346456*y(k-2);
 #define PID_K 1.0f
 #define PID_Ti 0.45f
 #define PID_Td 0.0f
-
+*/
 //% ziegler niestrojony
 //#define PID_K (0.6*PID_Kk)
 //#define PID_Ti (0.5*PID_Tu)
